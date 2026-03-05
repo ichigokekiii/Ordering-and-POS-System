7051197a70bd3ac5a0514f8b1b30c8e1fddc32e9
Cake <90282274+ichigokekiii@users.noreply.github.com>
2026-03-05 17:59:11 +0800

    user management

--- backend/database/migrations/2026_03_04_000001_create_users_table.php
+++ backend/database/migrations/2026_03_04_000001_create_users_table.php
@@ -23,6 +23,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
 
+            $table->text('address')->nullable(); 
             $table->string('phone_number')->nullable();
 
             $table->integer('failed_attempt_count')->default(0);

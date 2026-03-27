d02ecc944830b0eed215103ede6dbb35809acb03
gwynecabillo01 <gwynecabillo01@gmail.com>
2026-03-27 13:20:22 +0800

    edit/delete button premade admin

--- frontend/src/pages/admin/AdminProductPage.jsx
+++ frontend/src/pages/admin/AdminProductPage.jsx
@@ -335,6 +335,7 @@ function AdminProductPage({ user }) {
                   product={premade}
                   onEdit={handleEdit}
                   onDelete={handleDelete}
+                  canEdit={canEdit}
                 />
               ))}
             </div>
